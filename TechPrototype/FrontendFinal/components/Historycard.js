3fb320beb2bf76c106e58ce626c2c0f40f56d382
jiauri <wangjiarui4@outlook.com>
2022-04-26 21:22:58 +0800

    Add Link to Card and History Card

--- TechPrototype/FrontendFinal/components/Historycard.js
+++ TechPrototype/FrontendFinal/components/Historycard.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Box,
   Image,
@@ -10,23 +10,28 @@ import {
   Text
 } from 'native-base';
 
-import {Dimensions} from 'react-native';
-
+import { Dimensions } from 'react-native';
+import { PageSelectProvider } from "../utils/SwitchPage";
 const w = Dimensions.get('window').width;
 const h = Dimensions.get('window').height;
 
-const Historycard = () =>{
-    return ( <Box width={0.95 * w} direction="column" margin={0.02 * w} height={0.4 * w}>
-    <AspectRatio w="100%" ratio={{base: 25 / 9,md: 16 / 9}}>
+const Historycard = () => {
+  return <PageSelectProvider.Consumer>
+    {({ Page, SelectPage, Props }) => (<Box width={0.95 * w} direction="column" margin={0.02 * w} height={0.4 * w} onTouchEnd={() => {
+      Props = {};
+      SelectPage('detail');
+    }}>
+      <AspectRatio w="100%" ratio={{ base: 25 / 9, md: 16 / 9 }}>
         <Image source={{
-        uri: "https://scpic.chinaz.net/files/pic/pic9/201910/zzpic20739.jpg"
-        }} alt="image" borderRadius="xl"/>
-    </AspectRatio>
-    <Text mt={0.02 * w}  color="gray.400" bold size="xl">
+          uri: "https://scpic.chinaz.net/files/pic/pic9/201910/zzpic20739.jpg"
+        }} alt="image" borderRadius="xl" />
+      </AspectRatio>
+      <Text mt={0.02 * w} color="gray.400" bold size="xl">
         2022.4.16 上海交通大学
-    </Text>
+      </Text>
     </Box>
-    );
-  }
+    )}
+  </PageSelectProvider.Consumer>
+}
 
 export default Historycard;
